03f360a619cd82a2cecd96dcb0d1b09d680d3ab8
Travis Fowlston <travisfowlston@ymail.com>
2023-10-04 23:19:20 -0700

    finished script.js file

--- assets/script.js
+++ assets/script.js
@@ -15,35 +15,69 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page */
 
 // Assignment code here
-var chooseChar = function () {
+var chooseChar = function() {
   
-  var userChoiceLower = prompt("Do you want to include lowercase letters?");
+  var userChoiceLower = confirm("Do you want to include lowercase letters?");
     if (!userChoiceLower) {
       userChoiceLower = "";
     } else {
       userChoiceLower = "abcdefghijklmnopqrstuvwxyz";      
     }
 
-  var userChoiceUpper = prompt("Do you want to include UPPERCASE letters?");
+  var userChoiceUpper = confirm("Do you want to include UPPERCASE letters?");
     if (!userChoiceUpper) {
       userChoiceUpper = "";
     } else {
       userChoiceUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";      
     }
 
-  var userChoiceNum = prompt("Do you want to include numbers [0123456789]?");
+  var userChoiceNum = confirm("Do you want to include numbers [0123456789]?");
     if (!userChoiceNum) {
       userChoiceNum = "";
     } else {
       userChoiceNum = "0123456789";
     }  
   
-  var userChoiceSpecial = prompt("Do you want to include special characters [!@#$%?^&*]?");
+  var userChoiceSpecial = confirm("Do you want to include special characters [!@#$%?^&*]?");
     if (!userChoiceSpecial) {
       userChoiceSpecial = "";
     } else {
       userChoiceSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     }
+
+  var chooseString = userChoiceLower + userChoiceUpper + userChoiceNum + userChoiceSpecial
+
+  if (chooseString.length > 0) {
+    console.log("The string is " + chooseChar);
+    return chooseString;
+  } else {
+    window.alert("You must choose a minimum of at least one type of character.");
+    return chooseChar();
+  }
+}
+
+var createPassword = function() {
+  
+  /* Prompt user to choose the length of the password */
+  var chooseLength = prompt("How long would you like the password to be? Choose a number between 8 and 128!");
+  chooseLength = parseInt(chooseLength);
+  console.log("The choosen length is " + chooseLength);
+  
+  /* If not a number, length is less than 8, length is greater than 128, then return to beginning */
+  if (isNaN(chooseLength) || chooseLength < 8 || chooseLength > 128) {
+    return createPassword();
+  }
+
+  /* Creates the password based on the choosen criteria from the chooseChar function above */
+  var setCriteria = chooseChar();
+  var getPassword = "";
+  
+  /* Added a for loop using the Math.random function to generate a random combination based on the set criteria from the chooseChar function above */
+  for (let index = 0, a = setCriteria.length; index < chooseLength; index++) {
+    getPassword += setCriteria[Math.floor(Math.random() * a)];
+  }
+  console.log("The created password is " + getPassword);
+  return getPassword;
 }
 
 // Get references to the #generate element
@@ -51,7 +85,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
