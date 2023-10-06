950ba4908e2d7ae852627704ad20b4fc7ce044ca
Travis Fowlston <travisfowlston@ymail.com>
2023-10-05 21:35:44 -0700

    finished the script.js file and the README file

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # Module-3-Challenge - Password Generator
 
-This repository contains all work related to the Module 3 Challenge for the UC Davis Coding Bootcamp.
-Ref - https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+## Project Description
+
+The Module 3 Challenge objective was to create an application that an employee can use to generate a random password based on criteria they've selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+## Deployment Screenshot
+
+![deployment-screenshot](assets/images/deployment-screenshot.JPG)
+
+[Deployment Link]
+
+## Comments
+
+- Created a series of prompts for the password criteria which ask whether to include lowercase, UPPERCASE, numbers, and special characters.
+- Created a prompt to ask how many characters to include between the numbers 8 and 128.
+- Created stipulations that state you must select at least one character type.
+- Creates a password that is written to the page and is displayed in the container.
+
+## Credits
+
+- Starter code was provided by the UC Davis Bootcamp.
+- JavaScript was completed by Travis Fowlston.
+
+## License
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,25 @@
-/* GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page */
-
 // Assignment code here
+
+/* Created a function for all of the criteria */
 var chooseChar = function() {
   
-  var userChoiceLower = confirm("Do you want to include lowercase letters?");
-    if (!userChoiceLower) {
-      userChoiceLower = "";
-    } else {
-      userChoiceLower = "abcdefghijklmnopqrstuvwxyz";      
-    }
-
+  /* Used a confirm function to ask whether to include UPPERCASE letters */
   var userChoiceUpper = confirm("Do you want to include UPPERCASE letters?");
     if (!userChoiceUpper) {
       userChoiceUpper = "";
     } else {
       userChoiceUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";      
     }
-
+  
+  /* Used a confirm function to ask whether to include lowercase letters */
+  var userChoiceLower = confirm("Do you want to include lowercase letters?");
+    if (!userChoiceLower) {
+      userChoiceLower = "";
+    } else {
+      userChoiceLower = "abcdefghijklmnopqrstuvwxyz";      
+    }
+  
+  /* Used a confirm function to ask whether to include numbers */
   var userChoiceNum = confirm("Do you want to include numbers [0123456789]?");
     if (!userChoiceNum) {
       userChoiceNum = "";
@@ -38,6 +27,7 @@ var chooseChar = function() {
       userChoiceNum = "0123456789";
     }  
   
+  /* Used a confirm function to ask whether to include special characters */
   var userChoiceSpecial = confirm("Do you want to include special characters [!@#$%?^&*]?");
     if (!userChoiceSpecial) {
       userChoiceSpecial = "";
@@ -45,10 +35,11 @@ var chooseChar = function() {
       userChoiceSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     }
 
-  var chooseString = userChoiceLower + userChoiceUpper + userChoiceNum + userChoiceSpecial
+  /* Combines all the selected criteria from above */
+  var chooseString = userChoiceUpper + userChoiceLower + userChoiceNum + userChoiceSpecial
 
+  /* Checks to see if at least one of the criteria has been selected and gives an alert if criteria has not been satisfied */
   if (chooseString.length > 0) {
-    console.log("The string is " + chooseChar);
     return chooseString;
   } else {
     window.alert("You must choose a minimum of at least one type of character.");
@@ -56,12 +47,12 @@ var chooseChar = function() {
   }
 }
 
+/* Created a function to generate the password */
 var createPassword = function() {
   
   /* Prompt user to choose the length of the password */
   var chooseLength = prompt("How long would you like the password to be? Choose a number between 8 and 128!");
   chooseLength = parseInt(chooseLength);
-  console.log("The choosen length is " + chooseLength);
   
   /* If not a number, length is less than 8, length is greater than 128, then return to beginning */
   if (isNaN(chooseLength) || chooseLength < 8 || chooseLength > 128) {
@@ -76,7 +67,6 @@ var createPassword = function() {
   for (let index = 0, a = setCriteria.length; index < chooseLength; index++) {
     getPassword += setCriteria[Math.floor(Math.random() * a)];
   }
-  console.log("The created password is " + getPassword);
   return getPassword;
 }
 
@@ -89,7 +79,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
